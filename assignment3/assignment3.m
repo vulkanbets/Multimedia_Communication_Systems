31ca55cf37744b8e695e0e3b441852851e43f367
Juan Orlando <jcorlandomc@yahoo.com>
2021-04-10 19:31:54 -0700

    04/10/2021 07:31 pm

--- assignment3/assignment3.m
+++ assignment3/assignment3.m
@@ -21,23 +21,23 @@ for m=6:6
             if(x == 11) x_right_direction = 0; else x_right_direction = 8; end
             if(y == 1) y_up_direction = 0; else y_up_direction = 8; end
             if(y == 9) y_down_direction = 0; else y_down_direction = 8; end
-
+            
             search_x = ((x-1)*16) + 1 - x_left_direction;
             search_x_end = ((x-1)*16) + 16 + x_right_direction;
             search_y = ((y-1)*16) + 1 - y_up_direction;
             search_y_end = ((y-1)*16) + 16 + y_down_direction;
-
+            
             % This dynamically sets the boundaries for the search window in the !!!reference!!! frame depending on where the macroblock is
             search_window = reference_frame_y(search_y:search_y_end, search_x:search_x_end);
             % This dynamically sets the boundaries for the search window in the !!!reference!!! frame depending on where the macroblock is
-
+            
             % The extracted macro block from the target frame and the x and y coordinates
             MB = macro_blocks{y, x};
             MB_x = ((x-1)*16) + 1;
             MB_y = ((y-1)*16) + 1;
             % The extracted macro block from the target frame and the x and y coordinates
-
-
+            
+            
             % Determine the Best Match Macroblock
             min_SAD = inf;
             for i=1:length(search_window(1,:)) - 15
@@ -53,6 +53,7 @@ for m=6:6
                 end
             end
             % Determine the Best Match Macroblock
+            
         end
     end
     
